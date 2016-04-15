d06ccf202c9641c1cf0729279c9f0ab478cfae86
Nick <nick.van.eijk@itility.nl>
2016-04-15 15:14:24 +0200

    No need to pass mongoose, as it can just be required

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var util = require('util');
 var Logger = require('logger-facade-nodejs');
 var logger = Logger.getLogger('Mongoose');
+var mongoose = require('mongoose');
 
-function setMongooseLogs(mongoose) {
+function setMongooseLogs() {
   mongoose.set('debug', function(coll, method, query, doc, options) {
     var data = {
       coll: coll,
@@ -15,7 +16,7 @@ function setMongooseLogs(mongoose) {
     var query = JSON.stringify(data.query);
     var options = JSON.stringify(data.options || {});
 
-    var msg = util.format('db.%s.%s(%s, %s',
+    var msg = util.format('db.%s.%s(%s, %s)',
                           data.coll,
                           data.method,
                           query,
